bf47b30b095c96bf2b80cce5ac6a9006de66adfc
Conor McCarthy <conormccarthy@brainpool1.mynet>
2020-10-08 09:48:29 +0100

    update to linux build procedure

--- travis_setup.sh
+++ travis_setup.sh
@@ -7,13 +7,12 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   cp -r /usr/local/opt/librdkafka/lib ./cbuild
   cp -r /usr/local/opt/librdkafka/include ./cbuild
 elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  mkdir build && cd build
   git clone https://github.com/edenhill/librdkafka
   cd librdkafka
-  ./configure && make && sudo make install
-  cp -r /usr/local/lib/* ./cbuild
-  cp -r /usr/local/include/* ./cbuild
-  cd ../..
+  ./configure --prefix=../cbuild --disable-sasl --disable-lz4 --disable-ssl --mbits=64
+  make
+  sudo make install
+  cd ..
 else
   echo "$TRAVIS_OS_NAME is currently not supported"  
 fi
